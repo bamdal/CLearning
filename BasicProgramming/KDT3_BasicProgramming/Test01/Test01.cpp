2371b80b76e538b4ef14f4423f535e4fa9b8ea58
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-08-30 17:26:58 +0900

    간단한 함수구현 테스트

---
+++ BasicProgramming/KDT3_BasicProgramming/Test01/Test01.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+bool TestFunc(int Num, int N)
+{
+    bool result = false;
+    if (N !=0 && Num % N == 0)
+    {
+        result = true;
+    }
+
+    return result;
+}
+
+int main()
+{
+    int Num =0, N=0;
+    cout << "정수 Num 입력 : ";
+    cin >> Num;
+    cout << "\n정수 N 입력 : ";
+    cin >> N;
+
+    if (TestFunc(Num, N))
+    {
+        cout << Num << "은 " << N << "의 배수입니다";
+    }
+    else
+    {
+        cout << Num << "은 " << N << "의 배수가 아닙니다";
+    }
+
+
+
+}
