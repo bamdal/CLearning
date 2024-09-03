9c34862e0f97ba697aa42648f72f285a83f5dd0f
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-09-03 16:50:18 +0900

    형변환 / dynamic_cast<> static_cast<>로 성공여부 보면서 할수있음 자식 함수에 부모를 dynamic_cast를 시전하면 nullptr이 나오게 된다 반대로 부모함수에 자식을 dynamic_cast를 시전하면 자식은 new동적할당하면 어차피 부모도 전부 포인터로 가지고 있게되기에 null이 안나온다. 간단히 자식은 더 많은 정보를 가지고 있기에 캐스팅중 많은쪽에서 적은쪽은 되지만 반대는 안된다.

--- BasicProgramming/KDT3_BasicProgramming/L10_MordernTypeCasting/L10_MordernTypeCasting.cpp
+++ BasicProgramming/KDT3_BasicProgramming/L10_MordernTypeCasting/L10_MordernTypeCasting.cpp
@@ -3,6 +3,55 @@
 
 using namespace std;
 
+class CBase
+{
+public:
+	CBase();
+	virtual ~CBase();
+
+	 void Print()
+	{
+		cout << "CBase의 출력 함수" << endl;
+	}
+
+private:
+
+};
+
+CBase::CBase()
+{
+	cout << "CBase() 생성자 호출" << endl;
+}
+
+CBase::~CBase()
+{
+	cout << "~CBase() 소멸자 호출" << endl;
+}
+
+class CDerived : public CBase
+{
+public:
+	CDerived();
+	virtual ~CDerived() override;
+
+	 void Print()
+	{
+		cout << "CDerived의 출력 함수" << endl;
+	}
+private:
+
+};
+
+CDerived::CDerived()
+{
+	cout << "CDerived() 생성자 호출" << endl;
+}
+
+CDerived::~CDerived()
+{
+	cout << "~CDerived() 소멸자 호출" << endl;
+}
+
 int main()
 {
 #pragma region 형변환 / 명시적 형변환과 static_cast<>
@@ -70,6 +119,89 @@ int main()
 	}
 #pragma endregion
 
+#pragma region 형변환 / dynamic_cast<>
+	// dynamic_cast<> safe downcasting(안전한 다운캐스팅)에 사용
+	// => 베이스 클래스의 포인터에서 파생 클래스의 포인터로 다운캐스팅해주는 연산자
+	// => 다만 부모 클래스가 뭘 가리키고 있는지가 관건
+	// => 런타임 연산자기 때문에 캐스팅 비용이 조금 있다.
+	{
+		CBase* pBase = new CBase;
+		pBase->Print();
+
+		CBase* pDelete = pBase;
+		CDerived* Obj = new CDerived;	// pBase 가 파생이면 dynamic_cast<>의 성공
+		pBase = Obj;
+
+		//dynamic_cast<> 는 실행중에 유효하지 않은 포인터를 알려준다
+		CDerived* pDerived = dynamic_cast<CDerived*>(pBase);
+		if (pDerived == nullptr)
+		{
+			// CBase()를 통해 생성했기 때문 => 즉 CDerived 생성자를 호출한 곳이 없다.
+			cout << "Runtime Error!" << endl;
+		}
+		else
+		{
+			pDerived->Print();
+
+		}
+
+
+		delete pDelete;
+		delete pBase;
+		
+	}
+
+	{
+
+
+
+		CBase* pBase = new CDerived;
+		if (pBase == nullptr)
+		{
+			
+			cout << "Runtime Error!" << endl;
+		}
+		else
+		{
+			pBase->Print();	// 정상(베이스의 Print가 출력된다) => 만약 베이스만 virtual Print() 하면 파생 Print() 호출
+							// 만약 파생만 virtual이면 부모의 print 출력
+
+		}
+
+		CDerived* pDerived = dynamic_cast<CDerived*>(pBase);
+		if (pDerived == nullptr)
+		{
+			cout << "Runtime Error!" << endl;
+		}
+		else
+		{
+			// 파생 클래스로 생성했기 때문 => 즉, CDerived() 생성자가 호출 됬으니 다운 캐스팅이 가능
+			pDerived->Print();
+		}
+
+		delete pBase;
+	}
+
+	//dynamic_cast<> 업 캐스팅
+	{
+		CDerived* pDerived = new CDerived;
+		pDerived->Print();
+		// CDerived를 CBase로 형변환 시도 static_cast<> 성공체크하기
+		CBase* pBase = static_cast<CBase*>(pDerived);
+
+		if (pBase == nullptr)
+		{
+			cout << "Runtime Error!" << endl;
+		}
+		else
+		{
+			pBase->Print();	// 베이스 클래스의 Print()출력
+		}
+	
+
+		delete pDerived;
+	}
+#pragma endregion
 
 }
 
