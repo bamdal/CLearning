e3c082c15db083555e9b8a4b01704ea28ebe51ab
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-09-04 10:22:01 +0900

    L09_8_Friend.cpp : Friend클래스 & Friend함수 private에 있는 변수를 다른 함수에게 friend로 권한을 주어서 접근을 할 수있게 하는 함수다

---
+++ BasicProgramming/KDT3_BasicProgramming/L09_8_Friend/L09_8_Friend.cpp
@@ -0,0 +1,75 @@
+﻿// L09_8_Friend.cpp : Friend클래스 & Friend함수
+#include <iostream>
+#include <string>
+
+using namespace std;
+class MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+private:
+	string name;
+
+	friend void SetMyNamePlease(MyClass&, string);	// friend 함수 -> 맴버함수 아님 그냥 방명록 수준으로 그냥 이름만 적어놓은것
+													// 앞으로 이 시그니처의 SetMyNamePlease함수를 friend라고 미리 알려주는것
+	friend class Manager;	// Manager클래스는 friend
+
+	friend class Manager2;	// 현재 존재하지 않은 클래스를 지정해도 이상없다.
+};
+
+MyClass::MyClass()
+{
+	name = "default name";
+}
+
+MyClass::~MyClass()
+{
+}
+
+void SetMyNamePlease(MyClass& Obj, string str)
+{
+	Obj.name = str;
+	cout << "너의 이름은" << Obj.name << "이다." << endl;
+}
+
+
+class Manager
+{
+public:
+	Manager();
+	~Manager();
+
+	void SetYourName(MyClass& Obj, string str)
+	{
+		Obj.name = str;	// friend이기에 name에 접근 가능
+	}
+
+	void ShowYourName(MyClass& Obj)
+	{
+		cout << "내 이름은" << Obj.name << "이다." << endl;
+	}
+private:
+
+};
+
+Manager::Manager()
+{
+}
+
+Manager::~Manager()
+{
+}
+
+int main()
+{
+	MyClass me;
+	Manager manager;
+	manager.ShowYourName(me);
+	manager.SetYourName(me, "newName");
+	manager.ShowYourName(me);
+
+	int ii = 0;
+}
+
