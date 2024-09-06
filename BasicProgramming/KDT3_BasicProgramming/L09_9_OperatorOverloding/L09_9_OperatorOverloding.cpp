9c59bf0352aa2353b3e96307c1051e23634126db
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-09-06 10:56:01 +0900

    L09_9_OperatorOverloding.cpp :연산자 오버로딩 기존 operator들을 함수인것 처럼 판단해 오버로딩을 실행해 기존 연산자를 재정의 한다 operator + 자체가 함수명이라 판단하면 된다
    데이터 타입 변환에 대한 연산자 오버로딩
    클래스에서 어떠한 자료형으로 뭘 달라하면 뭘 줄지 미리 설정
    
    << 으로 출력하는것도 바꿀수있다

---
+++ BasicProgramming/KDT3_BasicProgramming/L09_9_OperatorOverloding/L09_9_OperatorOverloding.cpp
@@ -0,0 +1,33 @@
+﻿// L09_9_OperatorOverloding.cpp :연산자 오버로딩
+#include "Sample.h"
+#include <iostream>
+using namespace std;
+
+
+
+
+
+// 1) 클래스의 맴버 함수로 정의 하는 방법
+// 2) 전역 함수로 정의 하는 방법
+
+int main()
+{
+    {
+        cout << "[연산자 오버로딩]" << endl;
+
+        CTranslation T1(1, 2, 3);
+        T1.Print();
+        CTranslation T2 = { 4,5,6 };
+        T2.Print();
+        CTranslation T3 = T1 + T2;
+        T3.Print();
+        T3.SetN1(10);
+        T3.SetN2(5);
+
+        cout << endl << "[자료형 오버로딩]" << endl;
+        double dVal = T3;
+
+        cout << T1;
+
+    }
+}
---
+++ BasicProgramming/KDT3_BasicProgramming/L09_9_OperatorOverloding/Sample.h
@@ -0,0 +1,87 @@
+#pragma once
+#include <iostream>
+#include <format>
+
+using namespace std;
+
+class CTranslation
+{
+public:
+	CTranslation(double _x, double _y, double _z);
+	~CTranslation();
+
+	// 연산자 (+) 오버로딩
+	CTranslation operator + (const CTranslation& Obj);
+
+	// 데이터 타입 변환에 대한 연산자 오버로딩
+	operator double() const;
+
+	// 프랜드 함수로 << 오버로딩
+	friend ostream& operator << (ostream&, const CTranslation&);
+	
+	void SetN1(int n1);
+	void SetN2(int n2);
+
+	void Print()
+	{
+		cout << format("[X : {}, Y : {}, Z : {}]", X, Y, Z) << endl;
+	}
+private:
+	double X, Y, Z;
+	int N1, N2;
+	
+};
+
+CTranslation::CTranslation(double _x, double _y, double _z) : X(_x), Y(_y), Z(_z)
+{
+	N1 = N2 = 0;
+}
+
+CTranslation::~CTranslation()
+{
+}
+
+inline CTranslation CTranslation::operator+(const CTranslation& Obj)
+{
+	int x = X + Obj.X;
+	int y = Y + Obj.Y;
+	int z = Z + Obj.Z;
+
+	return CTranslation(x,y,z);
+}
+
+/// <summary>
+/// double 타입 변환 연산자 오버로딩
+/// CTranslation클래스에서 double 형으로 뭘 달라하면 뭘 줄지 미리 설정
+/// </summary>
+inline CTranslation::operator double() const
+{
+	if (N2 != 0)
+	{
+		return double(N1) / N2;
+
+	}
+	else
+	{
+		cout << "0나누기 불가";
+		return 0;
+	}
+}
+
+inline void CTranslation::SetN1(int n1)
+{
+	N1 = n1;
+}
+
+inline void CTranslation::SetN2(int n2)
+{
+	N2 = n2;
+}
+
+ostream& operator<<(ostream&, const CTranslation& T)
+{
+	return cout << format("[X : {}, Y : {}, Z : {}]\n", T.X, T.Y, T.Z);
+}
+
+
+
