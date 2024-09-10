0bd84d12c240b17b1c8dd09eb14153e98927c46f
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-09-10 17:42:32 +0900

    BFS하는중

--- BasicProgramming/KDT3_BasicProgramming/L17_Search/BFS.h
+++ BasicProgramming/KDT3_BasicProgramming/L17_Search/BFS.h
@@ -1,1 +1,55 @@
 #pragma once
+
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+typedef vector<vector<int>> VA;
+
+class BFS
+{
+	int NodeCount, EdgeCount, StartNode;
+	VA _va;
+	vector<bool> VArr;
+
+public:
+	BFS(int _cnt1, int _cnt2) : NodeCount(_cnt1), EdgeCount(_cnt2)
+	{
+		_va.resize(NodeCount + 1);
+		VArr = vector<bool>(NodeCount + 1, false);	// 방문 배열
+	}
+
+	VA& GetGraph() { return _va; }
+	
+	int F_Excute(int node)
+	{
+		queue<int> myqueue;
+		myqueue.push(node);
+		VArr[node] = true;
+		int count = 0;
+		while (!myqueue.empty())
+		{
+			int now_node = myqueue.front();
+			myqueue.pop();
+
+			cout << now_node << " ";
+			for (int i : _va[now_node])
+			{
+				if (!VArr[i])
+				{
+					count++;
+					VArr[i] = true;
+					myqueue.push(i);
+				}
+			}
+
+
+		}
+		return count;
+	}
+
+
+
+};
--- BasicProgramming/KDT3_BasicProgramming/L17_Search/L17_Search.cpp
+++ BasicProgramming/KDT3_BasicProgramming/L17_Search/L17_Search.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include "DFS.h"
+#include "BFS.h"
 
 using namespace std;
 
@@ -11,7 +12,7 @@ int main()
 #pragma region DFS (depth - first search)
 	{
 		bool go = true;
-		if (go)
+		if (!go)
 		{
 			int NodeCount, EdgeCount;
 			cout << "노드의 개수 입력 : ";
@@ -32,6 +33,36 @@ int main()
 			int cnt = ObjDFS.F_Execute();
 			cout << "DFS의 연결요소 수(그래프가 하나로 이뤄져 있는지)는 : " << cnt << "\n";
 		}
+	}
+#pragma endregion
+
+#pragma region BFS (breadth - first search)
+	{
+		bool go = true;
+		if (go)
+		{
+			int NodeCount, EdgeCount,Start;
+			cout << "노드의 개수 입력 : ";
+			cin >> NodeCount;
+			cout << "간선의 개수 입력 : ";
+			cin >> EdgeCount;
+			cout << "시작지점 : ";
+			cin >> Start;
+
+			BFS ObjBFS(NodeCount, EdgeCount);
+			for (int i = 0; i < EdgeCount; i++)
+			{
+				cout << "간선을 이루는 두 노드 입력 : " << endl;
+				int N1, N2;
+				cin >> N1 >> N2;	// Vi, Vj
+				ObjBFS.GetGraph()[N1].push_back(N2);
+				ObjBFS.GetGraph()[N1].push_back(N1);
+			}
+
+			int cnt = ObjBFS.F_Excute(Start);
+			cout << "BFS의 연결요소 수(그래프가 하나로 이뤄져 있는지)는 : " << cnt << "\n";
+		}
+
 	}
 #pragma endregion
 
